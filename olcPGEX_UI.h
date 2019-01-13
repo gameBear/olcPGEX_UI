d87c4d4195c213b0b6a7f6636f6759eafe744961
Michlas Cliff Lund-Mouritzen <Deva_i_tiden@hotmail.com>
2019-01-13 01:53:36 +0100

    Update olcPGEX_UI.h
    V 1.3
    Slider update: issue with slider handle position ig the Slider was moved. issue fixed.

--- olcPGEX_UI.h
+++ olcPGEX_UI.h
@@ -1,106 +1,106 @@
 /*
-	olcPGEX_UI.h v1.2
+	olcPGEX_UI.h v1.3
 
-	+-------------------------------------------------------------+
-	|   Extension made with OneLoneCoder Pixel Game Engine v1.12  |
-	|      "Simple UI Tool, or.. Simple'ish" - ZleapingBear       |
-	+-------------------------------------------------------------+
++-------------------------------------------------------------+
+|   Extension made with OneLoneCoder Pixel Game Engine v1.12  |
+|      "Simple UI Tool, or.. Simple'ish" - ZleapingBear       |
++-------------------------------------------------------------+
 
-	What is this?
-	~~~~~~~~~~~~~
-	This is an extension header for the OlcPixelGameEngine.
-	This extension is intented to help users quickly setup a protoptype for any game or program.
+What is this?
+~~~~~~~~~~~~~
+This is an extension header for the OlcPixelGameEngine.
+This extension is intented to help users quickly setup a protoptype for any game or program.
 
-	To this end, the extension provides two classes for direct use.
-	the UI_CONTAINER and the UI_STAGE_CONTROLL
+To this end, the extension provides two classes for direct use.
+the UI_CONTAINER and the UI_STAGE_CONTROLL
 
-	the first, UI_CONTAINER handles the updateing and drawing of all UI elements,
-	including bottons, drop down menus, text fields, sliders and backgroundfields, as well as asignment of a unique ID and communication with these.
-	For a single page application, such as a calculator or a stand-alone main menu this contain can be used and handles all objects by it self.
+the first, UI_CONTAINER handles the updateing and drawing of all UI elements,
+including bottons, drop down menus, text fields, sliders and backgroundfields, as well as asignment of a unique ID and communication with these.
+For a single page application, such as a calculator or a stand-alone main menu this contain can be used and handles all objects by it self.
 
-	The second class UI_STAGE_CONTROLL functions on the surface exactly as the UI_CONTAINER, with one exception.
-	The UI_STAGE_CONTROLL contains multiple UI_CONTAINERs at the same time and handles switching between them for the user,
-	this can even be done trough Commands bound to bottons at creation time.
+The second class UI_STAGE_CONTROLL functions on the surface exactly as the UI_CONTAINER, with one exception.
+The UI_STAGE_CONTROLL contains multiple UI_CONTAINERs at the same time and handles switching between them for the user,
+this can even be done trough Commands bound to bottons at creation time.
 
-	To help these classes there are multiple Enums created. the most important being the pgrSTAGE enum (Program stage)
-	this enum contains a list of 20 diffrent stages that teh program can be in, 10 predefined and 10 generic.
-	each stage name can also be used as a command on a botton, forcing the UI_STAGE_CONTROLL to switch to that stage if one such exists.
-
-
-
-	// All stages are:
-	// predefiend
-	RUN_INTRO, MAIN_MENU, RUN_PROGRAM, SAVE_LOAD,
-	PAUSE_PROGRAM, INVENTORY, VIEW_MAP, CREDITS,
-	HIGHSCORE, PGR_OPTIONS,
-	//and generic
-	PGR_STG0, PGR_STG1, PGR_STG2, PGR_STG3,
-	PGR_STG4, PGR_STG5, PGR_STG6, PGR_STG7,
-	PGR_STG8, PGR_STG9
-	
-	This PGEX will be updated when time is. pull requests and errors will be read and fixed when possible.
-	suggestions are welcome.
-
-	License notes:
-	~~~~~~~~~~~~~~~
-
-	Copyright 2018 Michlas C. Lund-Mouritzen
-
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
-
-	1. Redistributions or derivations of source code must retain the above
-	copyright notice, this list of conditions and the following disclaimer.
-
-	2. Redistributions or derivative works in binary form must reproduce
-	the above copyright notice. This list of conditions and the following
-	disclaimer must be reproduced in the documentation and/or other
-	materials provided with the distribution.
-
-	3. Neither the name of the copyright holder nor the names of its
-	contributors may be used to endorse or promote products derived
-	from this software without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+To help these classes there are multiple Enums created. the most important being the pgrSTAGE enum (Program stage)
+this enum contains a list of 20 diffrent stages that teh program can be in, 10 predefined and 10 generic.
+each stage name can also be used as a command on a botton, forcing the UI_STAGE_CONTROLL to switch to that stage if one such exists.
 
 
-	//////////////////////////////////////////////////////////////////////////////
-	Links
-	~~~~~
-		ZleapingBear:
-	Youtube:	https://www.youtube.com/zleapingbear
-	Twitter:	https://twitter.com/MCLMouritzen
-	GitHub:		https://github.com/gameBear/olcPGEX_UI/
 
-		OneLoneCoder:
-	YouTube:	https://www.youtube.com/javidx9
-	Discord:	https://discord.gg/WhwHUMV
-	GitHub:		https://www.github.com/onelonecoder
-	Homepage:	https://www.onelonecoder.com
+// All stages are:
+// predefiend
+RUN_INTRO, MAIN_MENU, RUN_PROGRAM, SAVE_LOAD,
+PAUSE_PROGRAM, INVENTORY, VIEW_MAP, CREDITS,
+HIGHSCORE, PGR_OPTIONS,
+//and generic
+PGR_STG0, PGR_STG1, PGR_STG2, PGR_STG3,
+PGR_STG4, PGR_STG5, PGR_STG6, PGR_STG7,
+PGR_STG8, PGR_STG9
+
+This PGEX will be updated when time is. pull requests and errors will be read and fixed when possible.
+suggestions are welcome.
+
+License notes:
+~~~~~~~~~~~~~~~
+
+Copyright 2018 Michlas C. Lund-Mouritzen
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions or derivations of source code must retain the above
+copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions or derivative works in binary form must reproduce
+the above copyright notice. This list of conditions and the following
+disclaimer must be reproduced in the documentation and/or other
+materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-	Relevant Videos
-	~~~~~~~~~~~~~~~
-	PGE
-	https://youtu.be/kRH6oJLFYxY Introducing olcPixelGameEngine
+//////////////////////////////////////////////////////////////////////////////
+Links
+~~~~~
+	ZleapingBear:
+Youtube:	https://www.youtube.com/zleapingbear
+Twitter:	https://twitter.com/MCLMouritzen
+GitHub:		https://github.com/gameBear/olcPGEX_UI
 
-	PGEX_UI
-	https://youtu.be/bfiSjC__MCI NotPong Demo example
+	OneLoneCoder:
+YouTube:	https://www.youtube.com/javidx9
+Discord:	https://discord.gg/WhwHUMV
+GitHub:		https://www.github.com/onelonecoder
+Homepage:	https://www.onelonecoder.com
 
-	Compiling in XXXX
-	~~~~~~~~~~~~~~~~~~
-	Sorry all, i have no way of testing this. but should run on all things PGE safe.
+
+Relevant Videos
+~~~~~~~~~~~~~~~
+PGE
+https://youtu.be/kRH6oJLFYxY Introducing olcPixelGameEngine
+
+PGEX_UI
+https://youtu.be/bfiSjC__MCI NotPong Demo example
+
+Compiling in XXXX
+~~~~~~~~~~~~~~~~~~
+Sorry all, i have no way of testing this. but should run on all things PGE safe.
 
 
 	Thanks
@@ -1188,9 +1188,15 @@ namespace olc
 				else if (localDir == RightLeft) pos = (float)((((float)length - (float)SliderHandle.getW()) / ((float)range.size() - 1)) * ((float)range.size() - (1 + (float)atStep)));
 				else if (localDir == LeftRight) pos = (float)((((float)length - (float)SliderHandle.getW()) / ((float)range.size() - 1)) *  (float)atStep);
 
-				if (localDir == BottomTop || localDir == TopBottom) SliderHandle.setY(position.y + pos);
+				if (localDir == BottomTop || localDir == TopBottom) {
+					SliderHandle.setY(position.y + pos);
+					SliderHandle.setY(position.x - SliderHandle.getW() * 0.5);
+				}
 
-				else SliderHandle.setX(position.x + pos);
+				else {
+					SliderHandle.setX(position.x + pos);
+					SliderHandle.setY(position.y - SliderHandle.getH() * 0.5);
+				}
 			}
 
 			void drawSlider(bool inDebugMode, std::vector<debugID> &storeDrawID) {
@@ -1244,8 +1250,14 @@ namespace olc
 			int getH() { return SliderHandle.getH(); }
 			int getLength() { return length; }
 
-			void setX(int newX) { position.x = newX; }
-			void setY(int newY) { position.y = newY; }
+			void setX(int newX) { position.x = newX; 
+				setSteppingSlider(getValueAsInt());
+
+			}
+			void setY(int newY) { position.y = newY; 
+				setSteppingSlider(getValueAsInt());
+
+			}
 			void setW(int newW) { 
 				SliderHandle.setW(newW);
 				if (localDir == TopBottom || localDir == BottomTop) {
